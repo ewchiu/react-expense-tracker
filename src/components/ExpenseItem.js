3f06ea67e52cb8a0c2cb60517a6bf13b110dd93b
Eric Chiu <ericwchiu@gmail.com>
2021-04-17 00:21:04 -0700

    working on tut

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,18 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-    return <h2>Expense item here!</h2>;
+    const expenseDate = new Date(2021, 3, 16);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 295.67;
+
+    return (
+        <div className="expense-item">
+            <div>{expenseDate.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">{expenseAmount}</div>
+            </div>
+        </div>);
 }
 
 export default ExpenseItem;
