335d50cc854cc2cde54712b7e383c9e61fe7f45d
Eric Chiu <ericwchiu@gmail.com>
2021-04-25 17:41:29 -0700

    added chart bars to expense tracker

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 
 import Card from '../UI/Card';
-import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart'
+import './Expenses.css';
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2021');
@@ -25,6 +26,7 @@ const Expenses = (props) => {
                     selected={filteredYear}
                     onChangeFilter={filterChangeHandler}
                 />
+                <ExpensesChart expenses={filteredExpenses}/>
                 <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
