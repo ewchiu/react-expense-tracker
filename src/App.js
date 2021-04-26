eb4759919f1d1cb68aa12f61585bad7ad83507fd
Eric Chiu <ericwchiu@gmail.com>
2021-04-25 17:56:41 -0700

    finished adding chart with bars

--- src/App.js
+++ src/App.js
@@ -6,27 +6,27 @@ import Expenses from './components/Expenses/Expenses';
 const DUMMY_EXPENSES = [
   { 
     id: 'e1', 
-    title: 'Car Insurance', 
-    amount: 295.67, 
-    date: new Date(2021, 4, 16) 
+    title: 'Protein Bar', 
+    amount: 22.64, 
+    date: new Date(2021, 3, 16) 
   },
   { 
     id: 'e2', 
     title: 'Toilet Paper', 
     amount: 13.45, 
-    date: new Date(2021, 5, 11) 
+    date: new Date(2021, 4, 11) 
   },
   { 
     id: 'e3', 
-    title: 'RTX 3070', 
-    amount: 605.67, 
-    date: new Date(2021, 4, 6) 
+    title: 'Gas', 
+    amount: 45.89, 
+    date: new Date(2021, 2, 6) 
   },
   {
     id: 'e4', 
     title: 'Gym Membership', 
     amount: 35.98, 
-    date: new Date(2021, 4, 18) 
+    date: new Date(2021, 1, 18) 
   }
 ];
 
